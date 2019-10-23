d8f62d7777bad0eb637cdbc6a4d5532c94181bdc
yesbabylon <nabu@yb.run>
2019-10-23 11:59:51 +0200

    added utf8 sanitation

--- public/packages/checkup/data/report-id.php
+++ public/packages/checkup/data/report-id.php
@@ -74,10 +74,19 @@ else {
     if(substr($url, -1) != '/') $url .= '/';
     curl_close($ch);
 
+
+    function sanitize_utf8($string, $replacement = '') {
+        return preg_replace('%(?:
+              \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+            | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+            | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+        )%xs', $replacement, $string);    
+    }
+    
     $report = Report::create([
                                 'domain'    => $domain, 
                                 'url'       => $url, 
-                                'content'   => $content
+                                'content'   => sanitize_utf8($content)
                             ])->first();
 
     $report_id = $report['id'];
