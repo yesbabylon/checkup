df146f9eed3d1f412af688529e71e602bbcfcd4a
yesbabylon <nabu@yb.run>
2019-10-24 15:12:57 +0200

    updates

--- public/packages/checkup/apps/src/controllers/homeController.js
+++ public/packages/checkup/apps/src/controllers/homeController.js
@@ -313,7 +313,7 @@ function($http, $scope, $rootScope, $location, $interval, $q, $uibModal, ngToast
                         
                     };
                 },
-                size: 'md',
+                size: 'sm',
                 appendTo: angular.element(document.querySelector('.modal-wrapper')),
             });
         };
